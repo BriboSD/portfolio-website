b060c9892da91a856d8f67087486a077b259596b
gscwin <Gschwinden23@gmail.com>
2025-10-04 02:42:43 -0700

    curtain effect intro added
    improved intro animation with curtain effect that iwll elda into the primary page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { motion, useAnimate } from "framer-motion";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const [scope, animate] = useAnimate();
+  const [showCurtain, setShowCurtain] = useState(true);
+  const [bg, setBg] = useState("black")
 
   useEffect(() => {
 
@@ -12,9 +14,9 @@ export default function Home() {
     const runSequence = async () => {
 
       // step 1: Fade in Hello
-      await animate("#hello", {opacity: 1, y:50}, {duration: 1.7});
+      await animate("#hello", {opacity: 1, y:50}, {duration: 1.5});
       // Step 2: Fade in My name
-      await animate("#intro-text", { opacity: 1, y:70 }, { duration: 0.6});
+      await animate("#intro-text", { opacity: 1, y:70 }, { duration: 0.8});
 
       //step 3: make intro text move up a little for spring effect
       await animate("#intro-text", {y:60 }, { duration: 0.2, delay: 0.3});
@@ -34,48 +36,66 @@ export default function Home() {
       //step 5: make intro text bounce again
       await animate("#intro-text", {y:285 }, { duration: 0.2});
 
+      await animate("#fullCurtain", {y:"0%"}, {duration: 1.5, delay: 1, ease: "easeInOut"}
+      ).then(()=>  {
+        setBg("gray");
+      });
     };
 
     runSequence();
   }, [animate]);
 
   return (
-    <main className="flex flex-col items-center justify-start pt-50 -min-h-screen relative" ref={scope}>
+    <main className="flex flex-col items-center justify-start pt-50 -min-h-screen relative"
+    ref={scope}
+    style={{ backgroundColor: bg }}
+    >
   
-  {/* Text stays in normal flow */}
-  <motion.h1
-    id="hello"
-    initial={{ opacity: 0, y: -100 }}
-    className="text-9xl font-bold z-10"
-  >
-    Hello.
-  </motion.h1>
+      {/* Text stays in normal flow */}
+      <motion.h1
+        id="hello"
+        initial={{ opacity: 0, y: -100 }}
+        className="text-9xl font-bold z-10"
+      >
+        Hello.
+      </motion.h1>
 
-  <motion.h1
-    id="intro-text"
-    initial={{ opacity: 0, y: 0 }}
-    className="text-4xl font-bold z-10 mt-4"
-  >
-    My name is Gabriel
-  </motion.h1>
+      <motion.h1
+        id="intro-text"
+        initial={{ opacity: 0, y: 0 }}
+        className="text-4xl font-bold z-10 mt-4"
+      >
+        My name is Gabriel
+      </motion.h1>
 
-  {/* Photo + mask absolutely positioned behind text */}
-  <div className="absolute top-[97%] left-1/2 -translate-x-1/2 mt-4 z-0">
-    <motion.img
-      id="photo"
-      src="/images/tempPic.png"
-      alt="Gabriel"
-      initial={{ opacity: 0, scale: 0.8, y: -10 }}
-      className="w-60 h-60 rounded-full border-2 border-blue-500"
-    />
+      {/* Photo + mask absolutely positioned behind text */}
+      <div className="absolute top-[365] left-1/2 -translate-x-1/2 mt-4">
+        <motion.img
+          id="photo"
+          src="/images/tempPic.png"
+          alt="Gabriel"
+          initial={{ opacity: 0, scale: 0.8, y: -10 }}
+          className="w-60 h-60 rounded-full border-2 border-blue-500 z-80"
+        />
 
-    <motion.div
-      id="photo-mask"
-      initial={{ height: "15rem" , y: -50}}
-      className="w-60 h-60 rounded-full absolute top-0 left-0"
-      style={{ backgroundColor: "#0a0a0a" }}
-    />
-  </div>
-</main>
+        <motion.div
+          id="photo-mask"
+          initial={{ height: "15rem" , y: -50}}
+          className="w-60 h-60 rounded-full absolute top-0 left-0"
+          style={{ backgroundColor: "#0a0a0a" }}
+        />
+      </div>
+
+      <div className="relative w-full h-screen" >
+        {showCurtain && (
+          <motion.div
+            id = "fullCurtain"
+            initial={{ y: "-100%" }}
+            onAnimationComplete={() => setShowCurtain(false)}
+            className="fixed top-0 left-0 w-full h-[200vh] bg-gray-500 z-50 pointer-events-none"
+          />
+        )}
+      </div>
+    </main>
   );
 }
